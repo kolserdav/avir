7617625a1dd9e7944bb7cedf53343f24e7771b85
kolserdav <serega12101983@gmail.com>
2018-03-11 01:50:02 +0800

    gf

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
+
+require '../vendor/autoload.php';
+
+use Avir\Re\Re;
+
+$re = new Re();
 $time1 = microtime();
 echo shell_exec("../a.out");
+$re->ass();
 $time2 = microtime();
 echo $time2 - $time1;
---
+++ src/Re/Re.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Avir\Re;
+
+class Re {
+
+    public function ass(){
+        $r = 434394859;
+        $l = 4837462637489;
+        $t = 3534545;
+        $s = $r/$t-$r+$l;
+        echo $s."<br>";
+    }
+}
