c9d46a90357411d001b24705f2166e19be0c6442
kolserdav <serega12101983@gmail.com>
2018-03-10 19:22:03 +0800

    twow

--- public/index.php
+++ public/index.php
@@ -1,7 +1,2 @@
 <?php
-// получает содержимое файла в строку
-$filename = "../a.out";
-$handle = fopen($filename, "r");
-$contents = fread($handle, filesize($filename));
-fclose($handle);
-var_dump($contents);
+echo "It works!";
