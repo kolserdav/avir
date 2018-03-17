34102ada62a2ac514ecbe76c06374272e31ab978
kolserdav <serega12101983@gmail.com>
2018-03-17 19:33:57 +0800

    link

--- public/index.php
+++ public/index.php
@@ -1,13 +1,6 @@
 <?php
-
+if ($_SERVER['REMOTE_ADDR'] == '188.162.77.160'){
 require '../vendor/autoload.php';
 
-use Avir\Re\Re;
 
-$re = new Re();
-$time1 = microtime();
-echo shell_exec("../a.out");
-$re->ass();
-$time2 = microtime();
-echo $time2 - $time1;
-echo '<br>tsest2w';
+}
