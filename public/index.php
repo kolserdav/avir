8510f2a1bbf8c8989e84df38df4f1e6b01e0e8ab
kolserdav <serega12101983@gmail.com>
2018-03-20 04:28:11 +0800

    res

--- public/index.php
+++ public/index.php
@@ -4,12 +4,6 @@ use Avir\Database\Modules\DBpdo;
 
 //var_dump( mail( 'serega12101983@gmail.com', '', 'This was sent with PHP.' ) ); // bool(true)
 
-$pdo = new DBpdo();
-//var_dump($pdo);
-$pdd = $pdo->getPDO();
-$pd = $pdd->query('INSERT INTO `baseAmazBase`.`users` (`name`, `email`) VALUES (\'sad\', \'as\')');
-var_dump($pd);
 
-if ($_SERVER['REMOTE_ADDR'] == '188.162.77.118' && $_SERVER['SERVER_ADDR'] == '127.0.0.1'){
     echo 1;
-}
+
